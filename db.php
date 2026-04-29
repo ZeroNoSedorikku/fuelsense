bba41a0409585316c5c7265201c88c4069210b5b
ZeroNoSedorikku <cedrikjanbicera@gmail.com>
2026-04-30 07:49:17 +0800

    Add files via upload

--- db.php
+++ db.php
@@ -1,7 +1,14 @@
 <?php
-$conn = pg_connect("host=localhost dbname=smartfuel_db user=postgres password=cedric");
+$conn = pg_connect("
+    host=dpg-d7obpthj2pic73digsk0-a.singapore-postgres.render.com
+    port=5432
+    dbname=fuelsense_db
+    user=fuelsense_db_user
+    password=QY6ZkIZT1WdTJP2U6NW9a1T01zIx1ZJN
+    sslmode=require
+");
 
 if (!$conn) {
-    echo "Connection failed!";
+    die("Database connection failed!");
 }
 ?>
