5a5050353c76d0762ff8de6ef3fb67c4a7c53810
ZeroNoSedorikku <cedrikjanbicera@gmail.com>
2026-04-29 11:28:17 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,5 @@
 <?php
-$conn = pg_connect("
-    host=dpg-d7obpthj2pic73digsk0-a.singapore-postgres.render.com
-    dbname=fuelsense_db
-    user=fuelsense_db_user
-    password=YOUR_PASSWORD
-");
+$conn = pg_connect("postgresql://fuelsense_db_user:QY6ZkIZT1WdTJP2U6NW9a1T01zIx1ZJN@dpg-d7obpthj2pic73digsk0-a.singapore-postgres.render.com/fuelsense_db?sslmode=require");
 
 if (!$conn) {
     die("Database connection failed!");
